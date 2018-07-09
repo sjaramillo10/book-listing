724f76cb9932d936d975fcc24b4ecf27b15aa390
Severiano Jaramillo <severiano.jaramillo@gmail.com>
2018-07-08 22:46:53 -0500

    Create Books loader class to fetch the books list from the Google Books API

---
+++ app/src/main/java/com/sjaramillo10/booklisting/Book.kt
@@ -0,0 +1,3 @@
+package com.sjaramillo10.booklisting
+
+data class Book(val title: String, val author: String)
---
+++ app/src/main/java/com/sjaramillo10/booklisting/BooksLoader.kt
@@ -0,0 +1,31 @@
+package com.sjaramillo10.booklisting
+
+import android.content.AsyncTaskLoader
+import android.content.Context
+
+/**
+ * Constructs a new [BooksLoader].
+ *
+ * @param context of the activity
+ * @param url to load data from
+ */
+class BooksLoader (context: Context, private val url: String?) :
+        AsyncTaskLoader<List<Book>>(context) {
+
+    companion object {
+
+        /** Tag for log messages  */
+        private val LOG_TAG = BooksLoader::class.java.name
+    }
+
+    override fun onStartLoading() {
+        forceLoad()
+    }
+
+    override fun loadInBackground(): List<Book>? {
+        return if (url == null) {
+            null
+        } else QueryUtils.fetchEarthquakeData(url)
+
+    }
+}
